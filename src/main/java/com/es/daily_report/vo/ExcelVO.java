98ce14b1ea440d25747371efbea1e7bab79f6da7
root <huang.zhou@excelsecu.com>
2021-10-29 16:02:48 +0800

    - 按照要求调整表格列顺序

--- src/main/java/com/es/daily_report/vo/ExcelVO.java
+++ src/main/java/com/es/daily_report/vo/ExcelVO.java
@@ -7,41 +7,33 @@ import lombok.Data;
 @Data
 @Builder
 public class ExcelVO {
-    @ExcelProperty("员工编号")
-    private String staffNo;
-
-    @ExcelProperty("员工姓名")
-    private String staffName;
-
-    @ExcelProperty("所属部门")
-    private String department;
-
-    @ExcelProperty("任务名")
-    private String taskName;
-
-    @ExcelProperty("任务耗时")
-    private String taskCost;
-
-    @ExcelProperty("任务详情")
-    private String taskDetail;
-
-    @ExcelProperty("项目编号")
-    private String projectNo;
-
-    @ExcelProperty("项目名")
-    private String projectName;
-
-    @ExcelProperty("产品编号")
-    private String productNo;
-
-    @ExcelProperty("产品名")
-    private String productName;
 
     @ExcelProperty("产品线")
     private String productLine;
 
+    @ExcelProperty("产品")
+    private String productName;
+
+    @ExcelProperty("部门")
+    private String department;
+
+    @ExcelProperty("员工姓名")
+    private String staffName;
+
+    @ExcelProperty("工时")
+    private String taskCost;
+
+    @ExcelProperty("项目")
+    private String projectName;
+
     @ExcelProperty("报告日期")
     private String reportDate;
+
+    @ExcelProperty("任务名")
+    private String taskName;
+
+    @ExcelProperty("任务详情")
+    private String taskDetail;
 
     @ExcelProperty("提交日期")
     private String commitDate;
